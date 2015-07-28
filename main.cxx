4f4c3fee50fb4ba9bfd2eedab58c789165c71560
Mincong <mincong.zhang@hotmail.com>
2015-07-28 21:18:25 +0100

    Update main.cxx

--- main.cxx
+++ main.cxx
@@ -3,21 +3,18 @@
 #include "SolarSystem.h"
 
 int main(){
-  SolarSystem sun("Sun");
+  SolarSystem sun("Sun",1);
   SolarSystem three_body("ThreeBody");
 
   while(true){
-    std::cout<<"-------New Round-------"<<std::endl;
-
     sleep(1);
-    sun.develop();
-    three_body.develop();
 
-    sun.explore(three_body);
-    three_body.explore(sun);
-
+    std::cout<<"-------New Report-------"<<std::endl;
+    sun.takeAction(three_body);
+    three_body.takeAction(sun);
 
     //Player can choose explore, develop, hide, do nothing etc. Comsuming diff resources respectively
+    //When exploring, have chance to find resources(higher tech, higher possibility)
 
     std::cout<<"-----------------------"<<std::endl;
 
