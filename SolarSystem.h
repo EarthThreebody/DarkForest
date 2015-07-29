3961d7c95bc54565a435a89a98f72fa0a9211cb6
Mincong <mincong.zhang@hotmail.com>
2015-07-29 18:04:49 +0100

    Update SolarSystem.h

--- SolarSystem.h
+++ SolarSystem.h
@@ -3,12 +3,6 @@
 
 #include <string>
 
-namespace SolarSystemAction{
-  const int DEVELOP = 1;
-  const int EXPLORE = 2;
-  const int NUM_OF_ACT = 2;
-}
-
 struct Position{double x,y;};
 
 class SolarSystem{
@@ -33,14 +27,10 @@ private:
   double m_explore_range;
   bool m_is_player;
 
-  int genRandom(const int & min, const int & max);
   void init();
   void report();
   void report(std::string new_report);
   void reportDetail();
-  void log(std::string out);
-  void log();
-  template <typename T> std::string to_string(T value);
 };
 
 #endif /* SOLAR_SYSTEM_H_INCLUDED */
